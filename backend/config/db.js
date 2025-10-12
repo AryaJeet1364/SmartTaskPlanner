78ad62d370f19756d3176738043bfd046b8b3a09
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-10-12 17:44:52 +0530

    Deployment changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,38 @@
 const mongoose = require("mongoose");
 
-let cached = global.mongoose;
-
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
-async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose
-      .connect(process.env.MONGO_URI, opts)
-      .then((mongoose) => {
-        return mongoose;
-      });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+    process.exit(1);
   }
+};
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+module.exports=connectDB;
 
-module.exports = connectDB;
+// let cached = global.mongoose;
+
+// if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+// async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//     };
+
+//     cached.promise = mongoose
+//       .connect(process.env.MONGO_URI, opts)
+//       .then((mongoose) => {
+//         return mongoose;
+//       });
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+// module.exports = connectDB;
