8fb249c0c0e92d0430f459a6c42c7c3bdc36bae5
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-10-12 11:19:28 +0530

    Deployment changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import TaskList from "./components/TaskList";
 
 // const API_URL = "http://localhost:5000/api";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL + "/api";
 
 
 export default function App() {
