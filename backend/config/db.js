04b5b4ec65155d57c20d678dacbb4f4b3e929065
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-10-12 17:00:05 +0530

    Deployment change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,39 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
-    process.exit(1);
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("✅ MongoDB connected");
+//   } catch (err) {
+//     console.error("❌ MongoDB connection error:", err);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+let cached = global.mongoose;
+
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
   }
-};
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 module.exports = connectDB;
