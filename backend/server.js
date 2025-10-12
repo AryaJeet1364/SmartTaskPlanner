ebd27da544cddec28154bfb46fde53c47d288e11
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-10-12 16:45:36 +0530

    Deployment change

--- backend/server.js
+++ backend/server.js
@@ -42,15 +42,15 @@ app.use(
   })
 );
 
-app.options(
-  "*",
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// app.options(
+//   "*",
+//   cors({
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
 
 
 
