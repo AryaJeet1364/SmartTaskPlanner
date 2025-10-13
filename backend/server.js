d46a5a1a636fb93d2998b9874a91f472eedf7ee4
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-10-13 15:32:36 +0530

    Last Deployment Change

--- backend/server.js
+++ backend/server.js
@@ -10,38 +10,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
-
-// const allowedOrigins = [
-//   process.env.FRONTEND_URL.replace(/\/$/, ""), // remove trailing slash
-//   "http://localhost:5173",
-//   "https://smart-task-planner-a2l6.vercel.app", // add your deployed frontend
-// ];
-
-// const allowedOrigins = [
-//   process.env.FRONTEND_URL?.replace(/\/$/, ""),
-//   "http://localhost:5173",
-//   "https://smart-task-planner-a2l6.vercel.app",
-// ].filter(Boolean); // removes any undefined
-
-// app.use((req, res, next) => {
-//   console.log("Origin header:", req.headers.origin);
-//   next();
-// });
-
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) callback(null, true);
-//       else callback(new Error("CORS not allowed"));
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
-
 
 app.use(
   cors({
@@ -52,23 +20,6 @@ app.use(
     credentials: true,
   })
 );
-
-
-// app.options(
-//   "*",
-//   cors({
-//     origin: allowedOrigins,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
-
-
-
-// ✅ Handle preflight requests explicitly
-// app.options("*", cors());
-
 
 app.use(express.json());
 
